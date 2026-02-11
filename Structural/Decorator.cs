ee3bc065b9fd31d02fb2f098eba12da9b33b25e2
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-02-11 11:44:12 +0600

    practice observer pattern

--- Structural/Decorator.cs
+++ Structural/Decorator.cs
@@ -1,100 +1,100 @@
-// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
+// // Online C# Editor for free
+// // Write, Edit and Run your C# code using C# Online Compiler
 
-using System;
+// using System;
 
-public interface ICoffe
-{
-    string GetDescription();
-    double GetCost();
+// public interface ICoffe
+// {
+//     string GetDescription();
+//     double GetCost();
 
-}
+// }
 
-public class SimpleCoffe: ICoffe
-{
-    public string GetDescription()
-    {
-        return "Simple Coffe";
-    }
-    public double GetCost()
-    {
-        return 5.0;
-    }
-}
-public abstract class CoffeDecorator : ICoffe
-{
-    public ICoffe _decoratedCoffe;
-    public CoffeDecorator(ICoffe coffe){
-        _decoratedCoffe=coffe;
-    }
+// public class SimpleCoffe: ICoffe
+// {
+//     public string GetDescription()
+//     {
+//         return "Simple Coffe";
+//     }
+//     public double GetCost()
+//     {
+//         return 5.0;
+//     }
+// }
+// public abstract class CoffeDecorator : ICoffe
+// {
+//     public ICoffe _decoratedCoffe;
+//     public CoffeDecorator(ICoffe coffe){
+//         _decoratedCoffe=coffe;
+//     }
     
-    public virtual string GetDescription()
-    {
-        return _decoratedCoffe.GetDescription() ;
-    }
-    public virtual double GetCost()
-    {
-        return _decoratedCoffe.GetCost();
-    }
+//     public virtual string GetDescription()
+//     {
+//         return _decoratedCoffe.GetDescription() ;
+//     }
+//     public virtual double GetCost()
+//     {
+//         return _decoratedCoffe.GetCost();
+//     }
     
-}
-public class MilkDecorator:CoffeDecorator{
+// }
+// public class MilkDecorator:CoffeDecorator{
     
-    public MilkDecorator(ICoffe coffe):base(coffe){
+//     public MilkDecorator(ICoffe coffe):base(coffe){
         
-    }
-    public override string GetDescription()
-    {
-        return _decoratedCoffe.GetDescription()+"Milk" ;
-    }
-     public override double GetCost()
-    {
-        return _decoratedCoffe.GetCost()+100;
-    }
-}
+//     }
+//     public override string GetDescription()
+//     {
+//         return _decoratedCoffe.GetDescription()+"Milk" ;
+//     }
+//      public override double GetCost()
+//     {
+//         return _decoratedCoffe.GetCost()+100;
+//     }
+// }
 
-public class SugarDectorator : CoffeDecorator
-{
-    public SugarDectorator(ICoffe coffe) : base(coffe)
-    {
-    }
+// public class SugarDectorator : CoffeDecorator
+// {
+//     public SugarDectorator(ICoffe coffe) : base(coffe)
+//     {
+//     }
 
-    public override string GetDescription()
-    {
-        return _decoratedCoffe.GetDescription() + " Sugar";
-    }
+//     public override string GetDescription()
+//     {
+//         return _decoratedCoffe.GetDescription() + " Sugar";
+//     }
 
-    public override double GetCost()
-    {
-        return _decoratedCoffe.GetCost() + 50;
-    }
-}
+//     public override double GetCost()
+//     {
+//         return _decoratedCoffe.GetCost() + 50;
+//     }
+// }
 
-public class HorilicsDectorator : CoffeDecorator
-{
-    public HorilicsDectorator(ICoffe coffe) : base(coffe)
-    {
-    }
+// public class HorilicsDectorator : CoffeDecorator
+// {
+//     public HorilicsDectorator(ICoffe coffe) : base(coffe)
+//     {
+//     }
 
-    public override string GetDescription()
-    {
-        return _decoratedCoffe.GetDescription() + " Horlics";
-    }
+//     public override string GetDescription()
+//     {
+//         return _decoratedCoffe.GetDescription() + " Horlics";
+//     }
 
-    public override double GetCost()
-    {
-        return _decoratedCoffe.GetCost() + 150;
-    }
-}
+//     public override double GetCost()
+//     {
+//         return _decoratedCoffe.GetCost() + 150;
+//     }
+// }
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-     ICoffe coffe=new SimpleCoffe();
-     Console.WriteLine($"{coffe.GetDescription()} - {coffe.GetCost()}");
-     ICoffe MilkCoffe=new MilkDecorator(coffe);
-     Console.WriteLine($"{MilkCoffe.GetDescription()} - {MilkCoffe.GetCost()}");
+// public class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//      ICoffe coffe=new SimpleCoffe();
+//      Console.WriteLine($"{coffe.GetDescription()} - {coffe.GetCost()}");
+//      ICoffe MilkCoffe=new MilkDecorator(coffe);
+//      Console.WriteLine($"{MilkCoffe.GetDescription()} - {MilkCoffe.GetCost()}");
 
-    }
-}
+//     }
+// }
